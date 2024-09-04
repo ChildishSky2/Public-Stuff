d692fd634c56a1da85a9b48c14dc44288ca6c8aa
ChildishSky2 <106922566+ChildishSky2@users.noreply.github.com>
2024-09-04 21:16:07 +0100

    Add files via upload

--- Graveler.py
+++ Graveler.py
@@ -1,30 +1,29 @@
-import random
 import multiprocessing
 import time
+from numpy.random import Generator, PCG64
+import numpy as np
 
-number_of_runs = 1_000_000_000
+number_of_runs = 10_000_000
+number_of_reps = 100
+
 
 number_of_concurrent = multiprocessing.cpu_count()
 args = [0 for _ in range(0, number_of_runs)]
 
+rng = Generator(PCG64())
 def count_values(_):
-    # Generate 231 random integers between 1 and 4
-    rolls = [random.randint(1, 4) for _ in range(231)]
-    
-    # Count occurrences of each value
-    counts = {value: rolls.count(value) for value in range(1, 5)}
-    
-    # Return the counts
-    return max(counts.values())
+    return np.count_nonzero(rng.integers(4, size=231) == 0)
+
 
 if __name__ == '__main__':
     multiprocessing.freeze_support()
     Pool = multiprocessing.Pool(processes = number_of_concurrent)
-
+    res = 0
     start = time.time()
-    res = max(Pool.map(count_values, args))
+    for _ in range(number_of_reps):
+        res = max(res, max(Pool.map(count_values, args)))
     end = time.time()
-    
+
     print("Highest Ones Roll:", res)
     print("Number of Roll Sessions:", number_of_runs)
     print("Time Taken(seconds):", end - start)
