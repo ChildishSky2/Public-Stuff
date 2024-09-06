fb4e050e0bc05508cb02ba0f7b6970f60db99aa9
ChildishSky2 <106922566+ChildishSky2@users.noreply.github.com>
2024-09-06 12:10:47 +0100

    Add files via upload

--- Graveler.py
+++ Graveler.py
@@ -5,22 +5,23 @@ start = datetime.now()
 
 best = 231
 
-rolls = 1_000_000
+repeats = 100
+rolls = 10_000_000
 length_of_run = 231
 
-array = np.random.randint(0, 4, size=(rolls, length_of_run), dtype=np.int8)
+for _ in range(repeats):
+    array = np.random.randint(0, 4, size=(rolls, length_of_run), dtype=np.int8)
 
-with tqdm(total=rolls) as pbar:
-    for array in array:
-        curr = np.count_nonzero(array)
-        if curr < best:
-            best = curr
-        pbar.update()
+
+    
+    curr = np.min(np.count_nonzero(array, axis = 1))
+    if curr < best:
+        best = curr
         if best < 54:
             break
 
 best = 231 - best
 
 print(f"Highest Roll: {best}")
-print(f"Number of Roll Sessions: {rolls}")
+print(f"Number of Roll Sessions: {rolls * repeats}")
 print(f"Took {datetime.now() - start} seconds to finish.")
